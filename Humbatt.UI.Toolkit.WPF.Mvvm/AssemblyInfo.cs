f4715100db75d6ab8929cc395bbe40869797cfe8
Dave Humphreys <dave@none.com>
2020-10-13 14:19:17 +0100

    Updated to build nuget packages and sign them

--- Humbatt.UI.Toolkit.WPF.Mvvm/AssemblyInfo.cs
+++ Humbatt.UI.Toolkit.WPF.Mvvm/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -8,3 +9,5 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: XmlnsDefinition("http://wpf.humbatt.com/winfx/xaml/controls", "Humbatt.UI.Toolkit.WPF.Controls", AssemblyName = "Humbatt.UI.Toolkit.WPF.Mvvm")]
