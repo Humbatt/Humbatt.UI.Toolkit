b0bcb61e31ec95e423b334417a0ffd1a24799e63
David H <newky2k@users.noreply.github.com>
2023-01-01 16:29:21 +0000

    Work on WInUI support

--- Humbatt.UI.Toolkit.WPF/Behaviours/DataGridSelectedItemsBlendBehavior.cs
+++ Humbatt.UI.Toolkit.WPF/Behaviours/DataGridSelectedItemsBlendBehavior.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+
 
 namespace Humbatt.UI.Toolkit.WPF.Behaviours
 {
