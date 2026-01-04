4466e68dca5cf2fe5d5baaa863d058f1685eeed0
David Humphreys <newky2k@users.noreply.github.com>
2026-01-04 21:30:55 +0000

    Work on the Option item extensions and helpers
    added test to WPF sample
    
    Migrated to slnx

---
+++ Humbatt.UI.Toolkit.Core/Extensions/OptionItemExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Humbatt.UI.Toolkit.Core.Models;
+
+namespace System;
+
+public static class OptionItemExtensions
+{
+    /// <summary>
+    /// Converts ienumerable to bindble object item list
+    /// </summary>
+    /// <typeparam name="T">Item type</typeparam>
+    /// <param name="target">Target list</param>
+    /// <param name="titleFunction">Function to return the title(Display Text) for eaxh item</param>
+    /// <param name="subTitleFunction">Optional function for returning the sub-title</param>
+    /// <returns></returns>
+    public static List<OptionItem<T>> BuildOptionItems<T>(this IEnumerable<T> target, Func<T, string> titleFunction, Func<T, string> subTitleFunction = null)
+    {
+        var options = new List<OptionItem<T>>();
+
+        foreach (var item in target)
+        {
+            var oi = new OptionItem<T>()
+            {
+                Value = item,
+            };
+
+            oi.Title = titleFunction(item);
+
+            if (subTitleFunction != null)
+            {
+                oi.SubTitle = subTitleFunction(item);
+            }
+
+            options.Add(oi);
+
+        }
+
+        return options;
+    }
+}
--- HumbattUIKitSample/MainWindow.xaml.cs
+++ HumbattUIKitSample/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Humbatt.UI.Toolkit.Core;
+using HumbattUIKitSample.Models;
 
 namespace HumbattUIKitSample
 {
@@ -46,5 +48,34 @@ namespace HumbattUIKitSample
             }
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var items = EnumHelper.BuildOptions<ItemTypes>();
+
+            ItemTypes[] exclude = [ItemTypes.NotDone];
+
+            var items2 = EnumHelper.BuildOptions(exclude);
+
+            Dictionary<ItemTypes, string> mapping = [];
+
+            mapping.Add(ItemTypes.NotDone, "Not Done");
+            mapping.Add(ItemTypes.Done, "Did Do");
+            mapping.Add(ItemTypes.Things, "Some Things");
+            mapping.Add(ItemTypes.MoreThings, "More Things");
+
+            var items3 = EnumHelper.BuildOptions(mapping);
+
+            var items4 = EnumHelper.BuildOptions(mapping, exclude);
+
+            var items5 = ViewModel.Items.BuildOptionItems(obj => obj.Name);
+
+            var items6 = ViewModel.Items.BuildOptionItems(obj => obj.Name, obj => "Sub-Title");
+
+            if (items6.Any())
+            {
+
+            }
+
+        }
     }
 }
